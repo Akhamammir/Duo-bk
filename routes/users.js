02c212c506ebf1c045009fa4c1556a2f1693b422
Akhamammir <it@paseusted.com.mx>
2020-12-14 20:12:53 -0600

    refix

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
-	console.log(req.body)
-  res.send('respond with a resource');
+router.post('/', function (req, res, next) {
+  console.log(req.body)
+  try {
+    let x = {
+      'root': {
+        pwd: 'toor'
+      }
+    }
+    console.log((x[req.body.usr].pwd == req.body.pwd ? true : false))
+    res.send({ auth: (x[req.body.usr].pwd == req.body.pwd ? true : false) });
+  } catch (Err) {
+    console.log(Err)
+  }
 });
+
 
 module.exports = router;
